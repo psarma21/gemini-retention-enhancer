dee9d492a95e1c2d3b554ea5faad7cb65bc047ac
Pawan Sarma <psarma21@gmail.com>
2025-04-10 22:36:46 -0700

    image update

--- wrapper/main.py
+++ wrapper/main.py
@@ -18,12 +18,22 @@ response_config=types.GenerateContentConfig(
         Bold key concepts and engaging words to highlight their significance and capture the user's attention respectively. Avoid empty new lines, try not to make sentences 
         too long in your response, and include relevant emojis in your response to capture the reader's attention."""
 )
+
+image_config=types.GenerateContentConfig(
+    temperature=1.0,
+    system_instruction="""These steps will be directly passed to an AI image generator so the steps need to be clear. The image should be creative and engaging, 
+    designed to help a novice computer science learner grasp the concept intuitively. For example, if the word is 'linked list', the description 
+    might include visual metaphors or illustrative elements, such as 'a long chain of nodes connected by arrows, each node containing a small icon of 
+    data inside. The goal is to create a vivid and imaginative representation that bridges theory with an accessible visual analogy. The AI generator 
+    cannot create text well so ideally the description should not ask to generate any text in the image. The image should be able to explain that word 
+    without any caption or text."""
+)
 model = "gemini-2.0-flash"
 
 news_api_key = os.getenv("NEWS_API_KEY")
 
-# add_context_to_prompt adds previous chat history to the prompt as context for Gemini
-def add_context_to_prompt(prompt, u1, g1, u2):
+# add_context_to_response_prompt adds previous chat history to the prompt as context for Gemini
+def add_context_to_response_prompt(prompt, u1, g1, u2):
     contents = []
     if u2:
         contents.append({
@@ -52,23 +62,17 @@ def add_context_to_prompt(prompt, u1, g1, u2):
 # modified query: vanilla query + modernization of vocabulary/language + customizable response (optional) + previous context to query
 def get_gemini_response(text, topic, last_user_prompt, last_gemini_response, second_to_last_user_prompt):
     try:
+       
         if topic != "none":
-            prompt = text + """. The previous sentence(s) are the user's initial prompt. Rewrite your response to make it more engaging and 
-            relatable for a novice CS student in college who is interested in """ + topic + """ . Be creative and modern in your 
-            explanation. Include a relevant diagram or other visual representation to enhance understanding, and provide a 
-            concise walkthrough explaining its significance and how it relates to the user's question. Only do this if a visual 
-            representation is needed. If applicable, ask a follow-up question at the end of your response that encourages deeper exploration 
-            of the topic, such as reviewing related code, learning a subtopic, or applying the concept practically. Avoid empty new lines in your response."""
-            prompt_with_context = add_context_to_prompt(prompt, u1=last_user_prompt, g1=last_gemini_response, u2=second_to_last_user_prompt)
-            response =  client.models.generate_content(model=model, contents=[prompt_with_context], config=response_config)  
+            prompt = text + ". I am a novice CS student in college who is interested in " + topic + ". "   
         else:
             prompt = text + ". I am a novice CS student in college."   
-            chat_history = add_context_to_prompt(prompt, last_user_prompt, last_gemini_response, second_to_last_user_prompt)       
-            response = client.models.generate_content(
-                model=model,
-                contents=chat_history,
-                config=response_config
-            )
+        query_with_context = add_context_to_response_prompt(prompt, last_user_prompt, last_gemini_response, second_to_last_user_prompt)       
+        response = client.models.generate_content(
+            model=model,
+            contents=query_with_context,
+            config=response_config
+        )
         return response.text
     except Exception as e:
         return f"Error: {e}"
@@ -97,20 +101,30 @@ def get_related_news(last_gemini_response, last_user_query):
     result =  client.models.generate_content(model=model, contents=[summarize_news_prompt+context], config=config)  
     return result.text
 
+# add_context_to_image_prompt adds previous chat history to the prompt as context for Gemini
+def add_context_to_image_prompt(oq, r, iq):
+    contents = []
+    contents.append({
+        "role": "user",
+        "parts": [{"text": oq}]
+    })
+    contents.append({
+        "role": "model",
+        "parts": [{"text": r}]
+    })
+    contents.append({
+        "role": "user",
+        "parts": [{"text": iq}]
+    })
+    return contents
+
 # enhancement 3 - call Gemini to generate a description for the bolded word
 def get_image_description_and_image(last_gemini_response, last_user_query, key_word):
-    image_query_prompt = """Given the user's question, your response, and specific word in your response, provide detailed steps to 
-    create an image that would visually represent that word that also enhances the understanding of the overlying concept discussed. 
-    These steps will be directly passed to an AI image generator so the steps need to be clear. The image should be creative and engaging, 
-    designed to help a novice computer science learner grasp the concept intuitively. For example, if the word is 'linked list', the description 
-    might include visual metaphors or illustrative elements, such as 'a long chain of nodes connected by arrows, each node containing a small icon of 
-    data inside. The goal is to create a vivid and imaginative representation that bridges theory with an accessible visual analogy. The AI generator 
-    cannot create text well so ideally the description should not ask to generate any text in the image. The image should be able to explain that word 
-    without any caption or text."""
-    caption_json_prompt = """Additionally, generate a concise and engaging caption that a novice learner can use to interpret and connect with the image
-    . For the entire response, use this JSON schema: {'image_description': str, 'caption': str}"""
-    context = " Here is the user's query: " + last_user_query + " . Here is your response: " + last_gemini_response + ' . Here is the key word: ' + key_word
-    image_description_response = client.models.generate_content(model=model, contents=[image_query_prompt + caption_json_prompt + context], config=config)
+    image_query_prompt = "Provide detailed steps to create an image that would visually represent the word" + key_word + "from your response that also enhances the understanding of the overlying concept discussed. "
+    caption_json_prompt = """Additionally, generate a concise and engaging caption that a novice learner can use to interpret 
+    and connect with the image. For the entire response, use this JSON schema: {'image_description': str, 'caption': str}"""
+    query_with_context = add_context_to_image_prompt(last_user_query, last_gemini_response, image_query_prompt+caption_json_prompt)
+    image_description_response = client.models.generate_content(model=model, contents=query_with_context, config=image_config)
     
     image_description_response_text = image_description_response.text
     cleaned_text = image_description_response_text.replace("```json", "").replace("```", "").strip()
